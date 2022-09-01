bd4dd780c9bab8685f776ba8d7b8297f2e4f45aa
zuzuqo <jesslich.nime@gmail.com>
2022-09-01 18:03:34 +0300

    1 - #3 Маршрутизация, обработка исключений

--- women/urls.py
+++ women/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from women.views import index
+from women.views import index, categories, archive
 
 urlpatterns = [
-    path('', index),
+    path('', index, name='home'),
+    path('categories/<int:cat_id>/', categories),
+    path('archive/<int:year>/', archive),
 ]
--- women/views.py
+++ women/views.py
@@ -1,6 +1,21 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound, Http404
+from django.shortcuts import render, redirect
 
 
 def index(request):
     return HttpResponse('Hello')
+
+
+def categories(request, cat_id):
+    return HttpResponse(f'Categories: {cat_id}')
+
+
+def archive(request, year):
+    if int(year) > 2022:
+        raise Http404()
+        # return redirect('home', permanent=True)
+    return HttpResponse(f'<h1>Архив по годам</h1><p>{year}</p>')
+
+
+def page_not_found(request, exception):
+    return HttpResponseNotFound('<h1>Страница не найдена</h1>')
