686a591bdfbecfcfeb2a49fb07c6eac2d95b6ee5
zuzuqo <jesslich.nime@gmail.com>
2022-09-01 21:02:27 +0300

    5 - #8 Формирование URL-адресов в шаблонах

--- women/urls.py
+++ women/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from women.views import index, about
+from women.views import index, about, add_article, contact, login, post_detail
 
 urlpatterns = [
     path('', index, name='home'),
     path('about/', about, name='about'),
+    path('add_article/', add_article, name='add_article'),
+    path('contact/', contact, name='contact'),
+    path('login/', login, name='login'),
+    path('post/<int:post_id>/', post_detail, name='post')
 ]
--- women/views.py
+++ women/views.py
@@ -3,19 +3,48 @@ from django.shortcuts import render, redirect
 
 from women.models import Women
 
-menu = ['О сайте', 'Добавить статью', 'Обратная связь', 'Войти']
+menu = [
+    {'title': 'Главная', 'url_name': 'home'},
+    {'title': 'О сайте', 'url_name': 'about'},
+    {'title': 'Добавить статью', 'url_name': 'add_article'},
+    {'title': 'Обратная связь', 'url_name': 'contact'},
+    {'title': 'Войти', 'url_name': 'login'},
+]
+
 
 def index(request):
     posts = Women.objects.all()
-    context = {}
-    context['menu'] = menu
-    context['posts'] = posts
-    return render(request, 'women/index.html', context)
+    context = {
+        'menu': menu,
+        'posts': posts,
+    }
+    return render(request, 'women/index.html', context=context)
 
+def post_detail(request, post_id):
+    post = Women.objects.get(pk=post_id)
+    context = {
+        'post': post,
+    }
+    return render(request, 'women/post_detail.html', context=context)
 
 def about(request):
-    return render(request, 'women/about.html')
+    context = {
+        'menu': menu,
+    }
+    return render(request, 'women/about.html', context=context)
 
 
 def page_not_found(request, exception):
     return HttpResponseNotFound('<h1>Страница не найдена</h1>')
+
+
+def add_article(request):
+    return HttpResponse('add_article')
+
+
+def contact(request):
+    return HttpResponse('contact')
+
+
+def login(request):
+    return HttpResponse('login')
